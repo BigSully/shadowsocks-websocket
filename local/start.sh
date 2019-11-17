9cd7dd50fa49590228522c5e1a7ad4e3560bc03a
george <george@example.com>
2019-11-17 16:55:50 +0800

    add start files

---
+++ local/start.sh
@@ -0,0 +1,8 @@
+
+export LOCAL_PORT=1080
+export SERVER=example.com
+export SERVER_PORT=443
+export PASSWORD=password
+nohup  ./ss-local-*  > ./ss-local.log 2>&1 &
+
+
---
+++ server/start.sh
@@ -0,0 +1,6 @@
+
+export SERVER_PORT=443
+export PASSWORD=password
+nohup  ./ss-server-*  > ./ss-server.log 2>&1 &
+
+
